bd02a52a2b0afdb2f506689f14af23400ff7106d
Nathan Wong <nathan.wong2002@gmail.com>
2023-09-13 20:33:31 -0600

    removed lines

--- script.py
+++ script.py
@@ -4,6 +4,5 @@ print(requests.__version__)
 
 #resp1 = requests.get("http://google.com")
 resp = requests.get("https://raw.githubusercontent.com/Eoljjang/Lab-1/main/script.py")
-r = requests.get("https://www.google.ca/")
 
 print(resp.text)
