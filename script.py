93cafceae6fe22ffe9e2870e7e855cc70117eadd
Nathan Wong <nathan.wong2002@gmail.com>
2023-09-13 18:15:19 -0600

    changed curl url request

--- script.py
+++ script.py
@@ -2,6 +2,7 @@ import requests
 
 print(requests.__version__)
 
-resp = requests.get("http://google.com")
+#resp = requests.get("http://google.com")
+resp = requests.get("https://raw.githubusercontent.com/Eoljjang/Lab-1/main/script.py?token=GHSAT0AAAAAACHR3XFIMMUQB5EC6ZPKAZAGZICKCHA")
 
 print(resp.text)
