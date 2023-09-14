21c15d530c62675e2e54fba68746f80b301789a4
Nathan Wong <nathan.wong2002@gmail.com>
2023-09-13 20:31:48 -0600

    changes

--- script.py
+++ script.py
@@ -2,7 +2,7 @@ import requests
 
 print(requests.__version__)
 
-#resp = requests.get("http://google.com")
+#resp1 = requests.get("http://google.com")
 resp = requests.get("https://raw.githubusercontent.com/Eoljjang/Lab-1/main/script.py")
 
 print(resp.text)
