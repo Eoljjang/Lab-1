e0fb7b18ab87cc688552a6d23286290f82e2baf2
Nathan Wong <99845938+Eoljjang@users.noreply.github.com>
2023-09-13 18:19:41 -0600

    Update script.py

--- script.py
+++ script.py
@@ -3,6 +3,6 @@ import requests
 print(requests.__version__)
 
 #resp = requests.get("http://google.com")
-resp = requests.get("https://raw.githubusercontent.com/Eoljjang/Lab-1/main/script.py?token=GHSAT0AAAAAACHR3XFIMMUQB5EC6ZPKAZAGZICKCHA")
+resp = requests.get("https://raw.githubusercontent.com/Eoljjang/Lab-1/main/script.py")
 
 print(resp.text)
